27070f457d879ccc735bb199db958a82746422f2
frostzzone <65735427+frostzzone@users.noreply.github.com>
2023-10-25 13:04:21 -0700

    Change

---
+++ plugins/fsh/README.md
@@ -0,0 +1,11 @@
+# Fsh api connect
+Use the fsh api from discord
+
+# How to install?
+Paste this plugin URL into the Plugins page of Vendetta:
+
+`https://frostzzone.github.io/venderta-mod/fsh`
+
+# Usage
+
+- nothing
--- plugins/fsh/src/index.ts
+++ plugins/fsh/src/index.ts
@@ -1,7 +1,48 @@
-import { logger } from "@vendetta";
+import { logger, commands } from "@vendetta";
+import { findByProps } from "@vendetta/metro";
 import Settings from "./Settings";
 
+const MessageActions = findByProps("sendMessage", "receiveMessage");
+const Locale = findByProps("Messages");
+
+let patches = [];
+
 export default {
+    onLoad: () => {
+        patches.push(commands.registerCommand({
+            name: "name",
+            displayName: "displayname",
+            description: "description",
+            displayDescription: "displayDescription",
+            options: [{
+                name: "message",
+                displayName: "message",
+                description: Locale.Messages.COMMAND_SHRUG_MESSAGE_DESCRIPTION,
+                displayDescription: Locale.Messages.COMMAND_SHRUG_MESSAGE_DESCRIPTION,
+                required: false,
+                // @ts-ignore
+                type: 3
+            }],
+            // @ts-ignore
+            applicationId: -1,
+            inputType: 1,
+            type: 1,
+        
+            execute: (args, ctx) => {
+                // args[0].value
+               
+                MessageActions.sendMessage(ctx.channel.id, {
+                    content: 'ok'
+                })
+            }
+        }));
+    },
+    onUnload: () => {
+        for (const unpatch of patches) unpatch()
+    }
+}
+
+/*export default {
     onLoad: () => {
         logger.log("Hello world!");
     },
@@ -9,4 +50,4 @@ export default {
         logger.log("Goodbye, world.");
     },
     settings: Settings,
-}
+}*/
